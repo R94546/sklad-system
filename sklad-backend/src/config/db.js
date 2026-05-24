9228ae29d47fd01eeb3acb7289826c2e79c7d846
R94546 <rustamabduvahobov5127@gmail.com>
2026-05-24 21:02:53 +0500

    fix: prisma client esm import

--- sklad-backend/src/config/db.js
+++ sklad-backend/src/config/db.js
@@ -1,8 +1,10 @@
-import 'dotenv/config';
-import { PrismaClient } from '@prisma/client';
+﻿import 'dotenv/config';
+import pkg from '@prisma/client';
+const { PrismaClient } = pkg;
 import { PrismaPg } from '@prisma/adapter-pg';
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 const prisma = new PrismaClient({ adapter });
 
 export default prisma;
+
