69808390af1dd67960447dd8d4d759b248e49a00
R94546 <rustamabduvahobov5127@gmail.com>
2026-05-25 13:33:29 +0500

    fix: production API URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+﻿import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://172.20.10.4:5000/api',
+  baseURL: 'https://sklad-system-production.up.railway.app/api',
 });
 
 api.interceptors.request.use((config) => {
